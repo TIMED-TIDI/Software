7db35a8e20177d37c676365e0d1d87d8f30dcc63
Aaron Ridley <ridley@umich.edu>
2021-03-31 11:57:40 -0400

    Can now do two types of plots, one showing alt vs LOS as a scatter plot, the other showing roughly an orbit's worth of LOS data from one telescope.

--- srcPython/tidi_los_read.py
+++ srcPython/tidi_los_read.py
@@ -19,16 +19,6 @@ date = ncfile.variables['ut_date']
 print(date)
 nlos, nchars = date.shape
 
-#times = []
-#for iLos in np.arange(nlos):
-#    d = ''
-#    for c in date[0][:]:
-#        d = d+c.decode("utf-8")
-#    times.append(dt.datetime.strptime(d,"%Y%j"))
-#print(times)
-
-
-
 time = np.double(np.array(ncfile.variables['time'][:]))
 print(time[0]/(365.25*86400.0))
 
@@ -41,29 +31,100 @@ print(last)
 
 nPts = 100
 
-sc_lat = np.array(ncfile.variables['sc_lat'][:])[0:nPts]
-sc_lon = np.array(ncfile.variables['sc_lon'][:])[0:nPts]
+iStart = 0
+iEnd = 12000
 
-tp_lat = np.array(ncfile.variables['tp_lat'][:])[0:nPts]
-tp_lon = np.array(ncfile.variables['tp_lon'][:])[0:nPts]
-tp_alt = np.array(ncfile.variables['tp_alt'][:])[0:nPts]
-print(tp_alt)
-
-tel_id = np.array(ncfile.variables['tel_id'][:])[0:nPts]
+sc_lat = np.array(ncfile.variables['sc_lat'][:])[iStart:iEnd]
+sc_lon = np.array(ncfile.variables['sc_lon'][:])[iStart:iEnd]
+tp_lat = np.array(ncfile.variables['tp_lat'][:])[iStart:iEnd]
+tp_lon = np.array(ncfile.variables['tp_lon'][:])[iStart:iEnd]
+tp_alt = np.array(ncfile.variables['tp_alt'][:])[iStart:iEnd]
+tel_id = np.array(ncfile.variables['tel_id'][:])[iStart:iEnd]
 print(tel_id)
+
+t_doppler = np.array(ncfile.variables['t_doppler'][:])[iStart:iEnd]
+t_rot = np.array(ncfile.variables['t_rot'][:])[iStart:iEnd]
+s_los = np.array(ncfile.variables['s'][:])[iStart:iEnd]
+s_var = np.array(ncfile.variables['var_s'][:])[iStart:iEnd]
+p_status = np.array(ncfile.variables['p_status'][:])[iStart:iEnd]
+table_index = np.array(ncfile.variables['table_index'][:])[iStart:iEnd]
+los_dir = np.array(ncfile.variables['los_direction'][:])[iStart:iEnd]
+
+print(los_dir)
+
+
+subtime = time[iStart:iEnd]
 
 ncfile.close()
 
-lon = sc_lon[sc_lon>0]
-lat = sc_lat[sc_lon>0]
-plt.scatter(lon, lat)
+angle = 45
 
-lon = tp_lon[sc_lon>0]
-lat = tp_lat[sc_lon>0]
-#col = tel_id[sc_lon>0]
-col = tp_alt[sc_lon>0]
-plt.scatter(lon, lat, c=col)
+# According to documents:
+# angles = 315 (5), 225 (4), 135 (3), 45 (2), 405 - cal (1)
 
+#plt.scatter(lon, lat)
+#
+tplon = tp_lon[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+tplat = tp_lat[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+tpalt = tp_alt[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+
+lon = sc_lon[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+lat = sc_lat[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+alt  = tpalt
+tdop = t_doppler[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+trot = t_rot[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+los = s_los[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+dir = los_dir[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+var = np.sqrt(s_var[(sc_lon>0) & (tel_id == angle) & (p_status == 0)])
+
+
+subsubtime = subtime[(sc_lon>0) & (tel_id == angle) & (p_status == 0)]
+
+times = []
+for t in subsubtime:
+    times.append(base + dt.timedelta(seconds = t))
+
+iPlotType = 0
+maxi = 500.0
+
+if (iPlotType == 0):    
+    
+    plt.plot(los,alt,'bo')
+    plt.xlim(-maxi,maxi)
+
+    for i,l in enumerate(los):
+        if (np.abs(l) < maxi):
+            a = alt[i]
+            v = var[i]
+            x = [l-v, l+v]
+            y = [a, a]
+            plt.plot(x,y,'r')
+    
+else:
+
+    #plt.plot(times,alt,'b.')
+
+    scale = 1.0 / 40.0
+    plt.plot(lon[(alt>250)],lat[(alt>250)],'bo')
+    plt.plot(tplon[(alt>250)],tplat[(alt>250)],'r.')
+
+    for i,l in enumerate(los):
+        a = alt[i]
+        if ((np.abs(l) < maxi) & (a>250)):
+            la = tplat[i]
+            lo = tplon[i]
+            d = dir[i] * np.pi / 180.0
+            dx = l * scale * np.sin(d) / np.abs(np.cos(lat[i] * np.pi / 180.0))
+            dy = l * scale * np.cos(d)
+            x = [lo, lo + dx]
+            y = [la, la + dy]
+            plt.plot(x,y,'r')
+
+            
+#plt.plot(var,alt,'r.')
+
+
+#print(times)
 
 #plt.scatter(tp_lon[sc_lon>0], tp_lat[sc_lon>0],'r.')
 plt.show()
